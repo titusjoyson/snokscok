247395a81882e29841e544e06b3f7dc87b9e8039
Titus Joyson <titus.joyson@imaginea.com>
2020-08-09 10:50:56 +0530

    Image Preview added

--- lib/screens/camera/components/miniPreview.dart
+++ lib/screens/camera/components/miniPreview.dart
@@ -2,14 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:snokscok/themes/const.dart';
 
 class MiniPreview extends StatelessWidget {
+  MiniPreview({this.onTap});
+  final Function onTap;
+
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(40),
-      decoration: BoxDecoration(
-        border: Border.all(color: Pallet.silkDark, width: 1.0),
+      child: Material(
         borderRadius: BorderRadius.circular(10),
-        color: Pallet.silkDark,
+        child: Ink(
+          decoration: BoxDecoration(
+            border: Border.all(color: Pallet.silkDark, width: 1.0),
+            borderRadius: BorderRadius.circular(10),
+            //color: Pallet.silkDark,
+            image: DecorationImage(
+              image: NetworkImage('https://picsum.photos/140/200/'),
+              fit: BoxFit.fill,
+            ),
+          ),
+          child: InkWell(
+            onTap: () {
+              this.onTap();
+            },
+          ),
+        ),
       ),
     );
   }
